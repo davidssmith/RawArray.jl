59a62071c3e94d8c78ec78da3954fe7b924bddba
David Smith <david.smith@gmail.com>
2017-05-04 16:24:48 -0500

    typo fix in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,13 @@ using Base.Test
 function test_wr(t, dims; compress=false)
   testfile = "tmp.ra"
   n = length(dims)
-  print("Testing $n-d $t $(compress ? "compressed" : "uncompressed") ...")
+  print("Testing $n-d $t $(compress ? "compressed" : "uncompressed") ... ")
   data = rand(t, dims...)
   rawrite(data, testfile; compress=compress)
   data2 = raread(testfile)
   @test isequal(data, data2)
   rm(testfile)
-  println("PASSED")
+  println("PASS")
 end
 
 typelist = [Float16, Float32, Float64, Complex32, Complex64, Complex128, Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]
